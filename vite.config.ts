3e914cf708b0a98081375833a968f300015b47fb
Mayank-1200 <mayankpansuriya86@gmail.com>
2025-11-27 15:46:19 +0530

    docs/ folder commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isDev = mode === 'development';
+    const repoBasePath = '/interex_industries/';
     return {
+      base: isDev ? '/' : repoBasePath,
       server: {
         port: 3000,
         host: '0.0.0.0',
