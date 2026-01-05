a6d49cd28376ca3bb0b759ba207d81c0076f6a64
Mayank <mayankpansuriya86@gmail.com>
2026-01-05 19:22:49 +0530

    idk just did it

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     const isDev = mode === 'development';
-    const repoBasePath = '/interex_industries/';
+    const repoBasePath = '/';
     return {
-      base: isDev ? '/' : repoBasePath,
+      base: repoBasePath,
       server: {
         port: 3000,
         host: '0.0.0.0',
